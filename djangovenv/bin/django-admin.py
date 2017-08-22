e881a6e7094551b08028fdda61cdd7d62d90c712
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-08-22 16:20:52 -0700

    for deploy

---
+++ djangovenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/blahmountain/Dev/Python/django-review/birthday_project/djangovenv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
