7ce432ab92125f9d35e3face362a64fadd8cb7d7
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-08-22 17:03:07 -0700

    fixed?

--- apps/login_app/models.py
+++ apps/login_app/models.py
@@ -37,26 +37,10 @@ class UserManager(models.Manager):
     user = User()
     user.first_name = postData['first_name'].capitalize()
     user.last_name = postData['last_name'].capitalize()
-    # user.b_date = time.strptime(postData['b_date'], "%Y-%m-%d")
     user.b_date = b_date
     user.email = postData['email']
     user.save()
     return user
-
-  def validation_login(self, postData):
-    results = {
-      'errors': [],
-    }
-    for data in postData:
-      if len(postData[data]) < 1 and str(data) != 'c_password':
-        results['errors'].append('{} field may not be blank'.format(data.capitalize()))
-    if User.objects.filter(email=postData['email']).count() < 1:
-        results['errors'].append('Email/Password not currently registered')
-    else:
-      hashed_pw = User.objects.filter(email=postData['email'])[0].password
-      if not bcrypt.checkpw(postData['password'].encode(), hashed_pw.encode()):
-        results['errors'].append('Email/Password not valid')        
-    return results
 
 class User(models.Model):
   first_name = models.CharField(max_length=100)
--- apps/login_app/views.py
+++ apps/login_app/views.py
@@ -8,15 +8,10 @@ def register(request):
     return render(request, 'login_app/register.html')
 
 def process_register(request):
-    try:
-        request.session['user_id']
-    except:
-        return redirect('/register')
     results = User.objects.validate_register(request.POST)
     if len(results['errors']) > 0:
         for error in results['errors']:
             messages.error(request, error, extra_tags='register')
         return redirect('/register')
     user = User.objects.add(request.POST)
-    request.session['user_id'] = user.id
     return redirect('/dashboard')
